d0ce2be25d3867ed2a3ab16e3fbf1d6fe981ccb8
Lucas De Marchi <lucas.de.marchi@gmail.com>
2009-07-08 02:00:58 +0200

    Add stuff to iterate through images from input dir
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

--- imgsum.c
+++ imgsum.c
@@ -18,17 +18,26 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
-
+#include <errno.h>
+#include <dirent.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <gtk/gtk.h>
-
-#include <dirent.h>
+#include <string.h>
 #include "parse_options.h"
 
+#ifdef DEBUG
+#define LOG(a, b)          \
+	if(options.verbose) \
+		printf(a, b)
+#else
+#define LOG(a,b) do { } while(0)
+#endif
+
 struct options_t options ={
+	.window       = 2,
 	.output_dir   = NULL,
 	.input_dir    = NULL,
 	.format       = "jpg",
@@ -39,7 +48,7 @@ struct options_t options ={
 	.verbose      = 0
 };
 
-static inline int isdir(char* path)
+static inline int is_dir(char* path)
 {
 	DIR* dir = opendir(path);
 	if(!dir)
@@ -48,20 +57,67 @@ static inline int isdir(char* path)
 	return 1;
 }
 
+/* Used by scandir function to select only image files */
+static int filter_images(const struct dirent *d)
+{
+	size_t len = strlen(d->d_name);
+	size_t ext_len = strlen(options.format);
+	char* ext;
+
+	if(ext_len >= len)
+		return 0;
+
+	ext = (char*)(d->d_name + len - ext_len);
+	if(!strcmp(ext, options.format))
+		return 1;
+	
+	return 0;
+}
+
 
 
 int main(int argc, char* argv[])
 {
 	int ret;
+	struct dirent **namelist;
+	int n_files;
+	unsigned int n_threads;
+
+	pthread_t* pool_threads;
+
+//init graphic library
 	gdk_init(&argc, &argv);
 
+//read options from command line
+	if((ret = getoptions(argc, argv, &options)) > 0)
+		return ret-1;
 
+//read list of filenames
+	n_files = scandir(".", &namelist, filter_images, alphasort);
+	if(n_files < 0){
+		perror("Couldn't open directory to scan files\n");
+		return 1;
+	}
+	LOG("Images: %d \n", n_files);
 
-	if((ret = getoptions(argc, argv, &options)) <=0)
-		return ret;
+//test output directory
+	if(!is_dir(options.output_dir)){
+		fprintf(stderr, "Couldn't access output directory\n");
+		return 1;
+	}
+
+//	n_threads = get_n_threads(n_files);
+//	pool_threads = malloc( sizeof(pthread_t) * n_threads);
+	for(int i = 0; i < n_files; i++){
+		printf("%s\n", namelist[i]->d_name);
+		free(namelist[i]);
+	}
+
+	free(namelist);
+
+	return 0;
 	// iterate through images
-
-// for_each_file_in_input_dir{
+	// for_each_file_in_input_dir{
 		GdkPixbuf* buf = NULL;
 		buf = gdk_pixbuf_new_from_file (argv[0], NULL);
 		
@@ -79,5 +135,4 @@ int main(int argc, char* argv[])
 
 
 	return 0;
-
 }
