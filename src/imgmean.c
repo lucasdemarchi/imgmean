6b2818c4d7569f064fa929a2d9eee7620b1312d1
Lucas De Marchi <lucas.de.marchi@gmail.com>
2009-07-10 18:45:47 +0200

    Name changes
    Signed-off-by: Lucas De Marchi <lucas.de.marchi@gmail.com>

--- src/imgmean.c
+++ src/imgmean.c
@@ -1,5 +1,5 @@
 /*
- * imgsum.c
+ * imgmean.c
  * This file is part of imgsum
  *
  * Copyright (C) 2009 - Lucas De Marchi
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Trissa; if not, write to the Free Software
+ * along with imgmean; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
--- src/parse_options.c
+++ src/parse_options.c
@@ -1,20 +1,20 @@
 /*
  * parse_options.c
- * This file is part of imgsum
+ * This file is part of imgmean
  *
  * Copyright (C) 2009 - Lucas De Marchi
  *
- * imgsum is free software; you can redistribute it and/or modify
+ * imgmean is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as published by
  * the Free Software Foundation.
  *
- * imgsum is distributed in the hope that it will be useful,
+ * imgmean is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Trissa; if not, write to the Free Software
+ * along with imgmean; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
